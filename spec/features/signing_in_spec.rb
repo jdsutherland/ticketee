864eccf96dd95d174bc293d259df006b0751fec5
Jeff Sutherland <jeffdsuth@gmail.com>
2018-08-07 18:04:08 +0700

    Add user sign in & sign out

---
+++ spec/features/signing_in_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature "Users can sign in" do
+  scenario 'when providing valid details' do
+    user = FactoryGirl.create(:user)
+
+    visit '/'
+    click_link 'Sign in'
+    fill_in 'Email', with: user.email
+    fill_in 'user_password', with: 'password'
+    click_button 'Log in'
+
+    expect(page).to have_content "Signed in successfully"
+    expect(page).to have_content "Signed in as #{user.email}"
+  end
+end
+
---
+++ spec/features/signing_out_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature "Users can sign out" do
+  scenario do
+    user = FactoryGirl.create(:user)
+    login_as(user)
+
+    visit '/'
+    click_link 'Sign out'
+
+    expect(page).to have_content "Signed out successfully"
+  end
+end
+
+
