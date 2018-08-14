3178fa2a180b629eef832a928aaca056d5338cb2
Jeff Sutherland <jeffdsuth@gmail.com>
2018-08-14 10:45:12 +0700

    Only show 'Edit Project' btn if user can update

--- spec/features/hidden_links_spec.rb
+++ spec/features/hidden_links_spec.rb
@@ -21,12 +21,34 @@ feature "Users can only see appropriate links" do
     end
   end
 
+  context "non-admin users (project viewers)" do
+    scenario 'cannot see the Edit Project Link' do
+      user = create(:user)
+      project = create(:project)
+      login_as(user)
+      assign_role!(user, :viewer, project)
+
+      visit project_path(project)
+
+      expect(page).to_not have_link "Edit Project"
+    end
+  end
+
   context 'admin users' do
     scenario 'can see the New Project Link' do
       login_as(create(:user, :admin))
       visit '/'
 
       expect(page).to have_link "New Project"
+    end
+
+    scenario 'can see the Edit Project Link' do
+      login_as(create(:user, :admin))
+      project = create(:project)
+
+      visit project_path(project)
+
+      expect(page).to have_link "Edit Project"
     end
 
     scenario 'can see the Delete Project Link' do
