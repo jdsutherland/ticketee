85de90fa8c8b727c414c1a551e05f70c0320e50f
Jeff Sutherland <jeffdsuth@gmail.com>
2018-08-14 17:23:55 +0700

    Add ability for file attachment to tickets

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'carrierwave', "~> 0.10.0"
 gem 'bootstrap-sass', '~> 3.3'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'devise', '~> 3.4.1'
--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -49,5 +49,24 @@ feature "Users can create new tickets" do
     expect(page).to have_content "Ticket has not been created."
     expect(page).to have_content "Description is too short"
   end
+
+  scenario "with an attachment" do
+    user = create(:user)
+    login_as(user)
+    project = create(:project, name: "Internet Explorer")
+    assign_role!(user, :editor, project)
+
+    visit project_path(project)
+    click_link 'New Ticket'
+    fill_in 'Name', with: 'Add documentation for speed tag'
+    fill_in 'Description', with: 'The blink tag has a speed attribute'
+    attach_file "File", "spec/fixtures/speed.txt"
+    click_button 'Create Ticket'
+
+    expect(page).to have_content "Ticket has been successfully created."
+    within('#ticket .attachment') do
+      expect(page).to have_content "speed.txt"
+    end
+  end
 end
 
