d9bc9a9075a66f7966c53c15d01161bdd8993e7a
Jeff Sutherland <jeffdsuth@gmail.com>
2018-08-14 15:50:42 +0700

    Roles assignable when admins create new users

--- spec/features/managing_roles_spec.rb
+++ spec/features/managing_roles_spec.rb
@@ -19,4 +19,20 @@ feature "Admins can manage a user's roles" do
     expect(page).to have_content "Internet Explorer: Viewer"
     expect(page).to have_content "vim: Manager"
   end
+
+  scenario "when assigning roles to a new user" do
+    login_as(create(:user, :admin))
+    ie = create(:project, name: "Internet Explorer")
+    vim = create(:project, name: "vim")
+
+    visit new_admin_user_path
+    fill_in 'Email', :with => 'newuser@ticketee.com'
+    fill_in 'Password', :with => 'password'
+    select 'Editor', from: 'Internet Explorer'
+    click_button 'Create User'
+    click_link 'newuser@ticketee.com'
+
+    expect(page).to have_content "Internet Explorer: Editor"
+    expect(page).not_to have_content "vim"
+  end
 end
