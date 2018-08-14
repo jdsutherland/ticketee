582a346f84a8eade0a3f5caa5490155d3072721a
Jeff Sutherland <jeffdsuth@gmail.com>
2018-08-14 13:23:00 +0700

    Hide New Ticket link if user can't create tickets

--- spec/features/hidden_links_spec.rb
+++ spec/features/hidden_links_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 feature "Users can only see appropriate links" do
   context 'regular users' do
-    scenario 'cannot see the New Project Link' do
+    scenario 'cannot see the New Project link' do
       login_as(create(:user))
       visit '/'
 
       expect(page).to_not have_link "New Project"
     end
 
-    scenario 'cannot see the Delete Project Link' do
+    scenario 'cannot see the Delete Project link' do
       user = create(:user)
       project = create(:project)
       login_as(user)
@@ -22,7 +22,7 @@ feature "Users can only see appropriate links" do
   end
 
   context "non-admin users (project viewers)" do
-    scenario 'cannot see the Edit Project Link' do
+    scenario 'cannot see the Edit Project link' do
       user = create(:user)
       project = create(:project)
       login_as(user)
@@ -32,17 +32,37 @@ feature "Users can only see appropriate links" do
 
       expect(page).to_not have_link "Edit Project"
     end
+
+    scenario 'cannot see the New Ticket link' do
+      user = create(:user)
+      project = create(:project)
+      login_as(user)
+      assign_role!(user, :viewer, project)
+
+      visit project_path(project)
+
+      expect(page).to_not have_link "New Ticket"
+    end
   end
 
   context 'admin users' do
-    scenario 'can see the New Project Link' do
+    scenario 'can see the New Project link' do
       login_as(create(:user, :admin))
       visit '/'
 
       expect(page).to have_link "New Project"
     end
 
-    scenario 'can see the Edit Project Link' do
+    scenario 'can see the New Ticket link' do
+      login_as(create(:user, :admin))
+      project = create(:project)
+
+      visit project_path(project)
+
+      expect(page).to have_link "New Ticket"
+    end
+
+    scenario 'can see the Edit Project link' do
       login_as(create(:user, :admin))
       project = create(:project)
 
@@ -51,7 +71,7 @@ feature "Users can only see appropriate links" do
       expect(page).to have_link "Edit Project"
     end
 
-    scenario 'can see the Delete Project Link' do
+    scenario 'can see the Delete Project link' do
       login_as(create(:user, :admin))
       project = create(:project)
       visit project_path(project)
