5f49dfce809487fbef9ca31e9d6f556be5ff37c6
Jeff Sutherland <jeffdsuth@gmail.com>
2018-08-17 15:58:33 +0700

    Add state-based searching for ticket

--- spec/features/searching_spec.rb
+++ spec/features/searching_spec.rb
@@ -7,11 +7,11 @@ feature "Users can search for tags matching specific criteria" do
     project = create(:project)
     assign_role!(author, :manager, project)
     ticket_1 = create(:ticket, name: 'Create doodad',
-                      project: project, author: author,
-                             tag_names: 'iteration_1')
+                               project: project, author: author,
+                               tag_names: 'iteration_1')
     ticket_2 = create(:ticket, name: 'Create ramrod',
-                      project: project, author: author,
-                      tag_names: 'iteration_2')
+                               project: project, author: author,
+                               tag_names: 'iteration_2')
 
     visit project_path(project)
     fill_in 'Search', with: 'tag:iteration_1'
@@ -22,5 +22,28 @@ feature "Users can search for tags matching specific criteria" do
       expect(page).not_to have_link 'Create ramrod'
     end
   end
-end
 
+  scenario 'searching by state' do
+    author = create(:user)
+    login_as(author)
+    project = create(:project)
+    assign_role!(author, :manager, project)
+    open = State.create(name: 'Open', default: true)
+    closed = State.create(name: 'Closed')
+    ticket_1 = create(:ticket, name: 'Create doodad', project: project,
+                               author: author, tag_names: 'iteration_1',
+                               state: open)
+    ticket_2 = create(:ticket, name: 'Create ramrod', project: project,
+                               author: author, tag_names: 'iteration_2',
+                               state: closed)
+
+    visit project_path(project)
+    fill_in 'Search', with: 'state:Open'
+    click_button 'Search'
+
+    within('#tickets') do
+      expect(page).to have_link 'Create doodad'
+      expect(page).not_to have_link 'Create ramrod'
+    end
+  end
+end
