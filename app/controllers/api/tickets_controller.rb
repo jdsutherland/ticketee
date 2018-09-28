123ba526915f3010eff5c43c111dbf1fb98a57ff
Jeff Sutherland <jeffdsuth@gmail.com>
2018-09-28 15:01:34 +0700

    Add error return on 401 & 403

--- app/controllers/api/tickets_controller.rb
+++ app/controllers/api/tickets_controller.rb
@@ -1,24 +1,35 @@
-class API::TicketsController < ApplicationController
-  before_action :set_project
-  before_action :authenticate_user
+module API
+  class TicketsController < ApplicationController
+    before_action :set_project
+    before_action :authenticate_user
 
-  attr_reader :current_user
+    attr_reader :current_user
 
-  def show
-    @ticket = @project.tickets.find(params[:id])
-    authorize @ticket
-    render json: @ticket
-  end
+    def show
+      @ticket = @project.tickets.find(params[:id])
+      authorize @ticket
+      render json: @ticket
+    end
 
-  private
+    private
 
-  def set_project
-    @project = Project.find(params[:project_id])
-  end
+    def set_project
+      @project = Project.find(params[:project_id])
+    end
 
-  def authenticate_user
-    authenticate_with_http_token do |token|
-      @current_user = User.find_by(api_key: token)
+    def authenticate_user
+      authenticate_with_http_token do |token|
+        @current_user = User.find_by(api_key: token)
+      end
+
+      if @current_user.nil?
+        render json: { 'error' => 'Unauthorized' }, status: 401
+        return # blank return halts filter chain & controller actions
+      end
+    end
+
+    def not_authorized
+      render json: { 'error' => 'Unauthorized' }, status: 403
     end
   end
 end
--- spec/requests/api/tickets_spec.rb
+++ spec/requests/api/tickets_spec.rb
@@ -24,4 +24,30 @@ RSpec.describe "Tickets API" do
       expect(response.status).to eq 200
       expect(response.body).to eq TicketSerializer.new(ticket).to_json
     end
-  end end
+
+    context "without permission to view the project" do
+      before do
+        user.roles.delete_all
+      end
+
+      it "responds with a 403" do
+        get api_project_ticket_path(project, ticket, format: "json"),
+            {}, headers
+
+        expect(response.status).to eq 403
+        error = { "error" => "Unauthorized" }
+        expect(JSON.parse(response.body)).to eq error
+      end
+    end
+  end
+
+  context "as an unauthenticated user" do
+    it "responds with a 401" do
+      get api_project_ticket_path(project, ticket, format: :json)
+
+      expect(response.status).to eq 401
+      error = { 'error' => 'Unauthorized' }
+      expect(JSON.parse(response.body)).to eq error
+    end
+  end
+end
